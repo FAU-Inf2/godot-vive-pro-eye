899affdedd3a16dfefa8e6c92676d8ff042bc216
Florian Jung <florian.jung@fau.de>
2019-07-16 14:08:35 +0200

    add more eye tracking functions

--- native/src/myclass.cpp
+++ native/src/myclass.cpp
@@ -11,7 +11,15 @@ using namespace std;
 using namespace ViveSR;
 
 void MyClass::_register_methods() {
-	register_method("_process", &MyClass::_process);
+	#define reg(name) register_method(#name, &MyClass::name)
+	reg(update_eye_data);
+	reg(get_eyeball_position);
+	reg(get_gaze_direction);
+	reg(is_user_present);
+	reg(get_timestamp);
+	reg(get_gaze_distance);
+	reg(get_pupil_size);
+	reg(get_eye_openness);
 }
 
 
@@ -49,30 +57,75 @@ MyClass::~MyClass() {
 	cout << "MyClass dtor" << endl;
 }
 
-void MyClass::update_eye_data()
+bool MyClass::update_eye_data()
 {
 	if (init_success)
 	{
+		int prev_frame = eye_data.frame_sequence;
 		int result = ViveSR::anipal::Eye::GetEyeData(&eye_data);
 		data_valid = (result == ViveSR::Error::WORK);
+
+		cout << "frame delta: " << eye_data.frame_sequence - prev_frame << endl;
 	}
 	else
 	{
 		data_valid = false;
 	}
+	return data_valid;
 }
 
-void MyClass::_process(float delta) {
-	update_eye_data();
-	
-	if (data_valid)
+
+const ViveSR::anipal::Eye::SingleEyeData* MyClass::get_eye(int eye)
+{
+	ViveSR::anipal::Eye::SingleEyeData* e;
+	switch(eye)
 	{
-		cout <<
-			eye_data.verbose_data.combined.eye_data.gaze_direction_normalized.x << "," <<
-			eye_data.verbose_data.combined.eye_data.gaze_direction_normalized.y << "," <<
-			eye_data.verbose_data.combined.eye_data.gaze_direction_normalized.z << "  |  " <<
-			eye_data.verbose_data.combined.eye_data.gaze_origin_mm.x << "," <<
-			eye_data.verbose_data.combined.eye_data.gaze_origin_mm.y << "," <<
-			eye_data.verbose_data.combined.eye_data.gaze_origin_mm.z << endl;
+		case -1: e = &eye_data.verbose_data.left; break;
+		case  1: e = &eye_data.verbose_data.right; break;
+		default: e = &eye_data.verbose_data.combined.eye_data; break;
 	}
+	return e;
+}
+
+static godot::Vector3 convert_vec(const ViveSR::anipal::Eye::Vector3& v)
+{
+	return godot::Vector3(-v.x, v.y, -v.z);
+}
+
+Vector3 MyClass::get_eyeball_position(int eye)
+{
+	return convert_vec(get_eye(eye)->gaze_origin_mm) / 1000.0;
+}
+
+Vector3 MyClass::get_gaze_direction(int eye)
+{
+	return convert_vec(get_eye(eye)->gaze_direction_normalized);
+}
+
+bool MyClass::is_user_present()
+{
+	return eye_data.no_user;
+}
+
+double MyClass::get_timestamp()
+{
+	return eye_data.no_user;
+}
+
+double MyClass::get_gaze_distance()
+{
+	if (eye_data.verbose_data.combined.convergence_distance_validity)
+		return eye_data.verbose_data.combined.convergence_distance_mm / 1000.;
+	else
+		return -1.;
+}
+
+double MyClass::get_pupil_size(int eye)
+{
+	return get_eye(eye)->pupil_diameter_mm;
+}
+
+double MyClass::get_eye_openness(int eye)
+{
+	return get_eye(eye)->eye_openness;
 }
--- native/src/myclass.h
+++ native/src/myclass.h
@@ -19,11 +19,47 @@ private:
 
 	ViveSR::anipal::Eye::EyeData eye_data;
 
+	const ViveSR::anipal::Eye::SingleEyeData* get_eye(int eye);
+
 public:
 	static void _register_methods();
 	
-	void _process(float delta);
-	void update_eye_data();
+	/** Must be called each frame before get_eyeball_position() or
+	    get_gaze_direction() is called. Returns true on success or
+		false on error. In the error case, the get_* methods return
+		garbage data.
+	*/
+	bool update_eye_data();
+
+	/** returns the eye gaze origin in meters.
+	    The Vector3 returned follows the godot convention, i.e.
+		right/up/viewing direction = +x / +y / -z.
+		Note that this is different from the SRanipal convention.
+	*/
+	Vector3 get_eyeball_position(int eye);
+
+	/** returns the normalized eye gaze direction.
+	    The Vector3 returned follows the godot convention, i.e.
+		right/up/viewing direction = +x / +y / -z.
+		Note that this is different from the SRanipal convention.
+	*/
+	Vector3 get_gaze_direction(int eye);
+
+	/** returns whether a user is present */
+	bool is_user_present();
+
+	/** returns the timestamp of the current eye data */
+	double get_timestamp();
+
+	/** returns the estimated gaze distance based on the convergence
+	    of the two eyes. returns -1 if unknown */
+	double get_gaze_distance();
+
+	/** returns the pupil diameter in mm */
+	double get_pupil_size(int eye);
+
+	/** returns whether the eye is open (1) or closed(0) or in between */
+	double get_eye_openness(int eye);
 
 	MyClass();
 	~MyClass();
